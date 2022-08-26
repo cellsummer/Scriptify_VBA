4b285abdb91b31fee2a2a2a2e42fd1b2053b09da
Wenjing Fang <fang.wenjing@yahoo.com>
2022-08-26 16:22:45 -0400

    excel copy funcs

---
+++ pde.py
@@ -0,0 +1,38 @@
+from core_funcs import ExcelData
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def from_dev_sql(file_path: str):
+    data = ExcelData()
+    data.load_data_sql(file_path)
+    data.df_to_curr_xl()
+    print('Data from sql has been pasted into Excel')
+
+    return 0
+
+
+@app.command()
+def from_test_sql(file_path: str):
+    data = ExcelData()
+    data.load_data_sql(file_path, server='TEST')
+    data.df_to_curr_xl()
+    print('Data from sql has been pasted into Excel')
+
+    return 0
+
+
+@app.command()
+def from_csv(file_path: str):
+    data = ExcelData()
+    data.load_data_file(file_path)
+    data.df_to_curr_xl()
+    print('Data from csv has been pasted into Excel')
+
+    return 0
+
+
+if __name__ == '__main__':
+    app()
