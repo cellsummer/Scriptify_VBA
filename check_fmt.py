b9466514047e39d8a653a3415497f6268721b73d
Wenjing Fang <fang.wenjing@yahoo.com>
2022-11-13 14:28:19 -0500

    script for checking fmt

---
+++ check_fmt.py
@@ -0,0 +1,84 @@
+import os
+import json
+import pandas as pd
+
+
+class FMT:
+    def __init__(
+        self,
+    ):
+        pass
+
+    def _load_config(self, config: str):
+        if not os.path.exists(config):
+            raise FileNotFoundError("File: {config} doesn't exist.")
+
+        with open(config, "r") as file:
+            fmt_config = json.load(file)
+
+        print(f"{config} has been loaded")
+        print(fmt_config)
+
+    def _load_pit(
+        self,
+    ):
+        pass
+
+    def _load_fmt(
+        self,
+    ):
+        pass
+
+    def _get_combos(self, df, columns, sep="|"):
+        """
+        return a list of combos from a dataframe
+        """
+        if df.empty:
+            return []
+
+        if columns is None:
+            msg = "Empty list of columns specified in function get_combo"
+            raise Exception(msg)
+            return
+
+        if any(col not in df.columns for col in columns):
+            msg = "Some of the tags don't exist in the columns of dataframe."
+            raise Exception(msg)
+            return
+
+        combos = [
+            sep.join(
+                [str(_) for _ in df.loc[row_no, columns]]
+            ) for row_no in df.index
+        ]
+
+        return combos
+
+    def get_pit_combos(tab: str):
+        pass
+
+    def get_fmt_combos(tab: str):
+        pass
+
+
+def main():
+    df = pd.DataFrame(
+        data={
+            "age": [34, 33, 40],
+            "gender": ["m", "f", "m"],
+            "rating": ["R0", "R0", "R1"],
+        }
+    )
+    cols = ['age', 'gender', 'rating']
+    cols_wrong = ['ddd']
+    my_fmt = FMT()
+    combos = my_fmt._get_combos(df, cols[:2])
+    # for row_no in df.index:
+    #     row = df.loc[row_no, :]
+    #     for key in row:
+    #         print(key)
+    print(combos)
+
+
+if __name__ == "__main__":
+    main()
