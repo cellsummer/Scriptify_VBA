c0d30ebd2baa3e3490000060b4a8a68c3b2efff6
Wenjing Fang <fang.wenjing@yahoo.com>
2023-01-02 21:10:30 -0500

    main.py & main-gui.py

--- src/apps/cfs.py
+++ src/apps/cfs.py
@@ -9,7 +9,8 @@
 # standardize column name for scenario number and projetion month
 import re
 import pandas as pd
-from utils.utils import AttributeDict, read_config
+from utils.utils import AttributeDict, read_config, logger
+from typing import Dict
 
 
 class CFs:
@@ -32,6 +33,7 @@ class CFs:
         self.df = df
         self.rename_df_columns()
         self.clean_params()
+        self.grouped_cfs = []
 
     def clean_params(
         self,
@@ -98,7 +100,10 @@ class CFs:
         # temp column for the weighted cashflows
         suffix = "weighted_"
 
-        for cf_name in self.params.cfs:
+        # raw cashflows + goruped cashflows
+        all_cfs = self.params.cfs + self.grouped_cfs
+
+        for cf_name in all_cfs:
             weighted_cf_name = suffix + cf_name
             self.df[weighted_cf_name] = (
                 self.df[cf_name] * self.df[self._COL_ESS_WEIGHTS]
@@ -107,16 +112,16 @@ class CFs:
         ess_df = (
             self.df.loc[
                 self.filter_is_stochastic,
-                [self._COL_PROJ_MTH] + [suffix + col for col in self.params.cfs],
+                [self._COL_PROJ_MTH] + [suffix + col for col in all_cfs],
             ]
             .groupby(self._COL_PROJ_MTH)
             .sum()
         )
 
         # drop temp weighted columns
-        self.df.drop(columns=[suffix + col for col in self.params.cfs], inplace=True)
+        self.df.drop(columns=[suffix + col for col in all_cfs], inplace=True)
 
-        rename_mapper = {suffix + col: col for col in self.params.cfs}
+        rename_mapper = {suffix + col: col for col in all_cfs}
         ess_df.rename(columns=rename_mapper, inplace=True)
 
         # set scenario number to be -1
@@ -138,14 +143,17 @@ class CFs:
 
         return
 
-    def calc_pvs(
+    def _calc_pvs(
         self,
     ) -> pd.DataFrame:
         """calculate pv of cashflows by scenario"""
         results = AttributeDict()
         pv_col = "pv"
 
-        for col in self.params.cfs:
+        # raw cashflows + goruped cashflows
+        all_cfs = self.params.cfs + self.grouped_cfs
+
+        for col in all_cfs:
             self.df[pv_col] = self.df[col] * self.df[self._COL_DISCOUNT_FACTOR]
             results[col] = (
                 self.df.loc[:, [pv_col, self._COL_SCENARIO_NUMBER]]
@@ -156,6 +164,72 @@ class CFs:
         self.df.drop(columns=pv_col, inplace=True)
 
         return pd.DataFrame(data=results)
+
+
+    def group_cfs(self, groupings: list[str]) -> None:
+        '''group cashflows use the grouping defined in the params
+        Args:
+            groupings (list): list of groupings to add to the dataframe 
+        Returns:
+            None: new columns are added to self.df. new columns are named as <grouping_def>.<group_name>
+        '''
+        for grouping_def in groupings:
+            # check if it is defined in params
+            if grouping_def not in self.params.cfs_groupings:
+                logger.warn(f'{grouping_def} is not defined in params: Ignored')
+                continue
+            
+            for local_group_name, group_def in self.params.cfs_groupings[grouping_def].items():
+                global_group_name = f'{grouping_def}.{local_group_name}'
+                self._add_cfs_group(global_group_name, group_def)
+
+        return
+    
+    def _add_cfs_group(self, group_name: str, group_def: Dict[str, int]) -> None:
+        '''add an aggregate of cashflows to self.df
+        Args:
+            group_name (str): the suffix added to the new cashflow group name
+            group_def (dict): definition of the cashflow group. Key is cashflow name. Value is sign.
+        Returns:
+            None: A new columns is added to self.df, named as group_name. 
+        '''
+
+        self.df[group_name] = 0
+
+        for cf, sign in group_def.items():
+            self.df[group_name] += self.df[cf] * sign
+
+        # update grouped cfs 
+        self.grouped_cfs.append(group_name)
+
+        return
+
+    def calc_bels(self, bel_definitions: list[str]) -> pd.DataFrame:
+        '''Calculate Bels in each scenario
+        Args:
+            bel_definitions (list): list of bel definitions
+        Returns:
+            pd.DataFrame: dataframe (index is scenario, column is bel defintions)
+        '''
+        pvs = self._calc_pvs()
+        for bel_def_name in bel_definitions:
+            # loop through all bel_defintions
+            if bel_def_name not in self.params.bel_definitions:
+                # bel definition not in params, do nothing
+                logger.warn(f"{bel_def_name} is not defined in self.params")
+                continue
+            
+            bel_def = self.params.bel_definitions[bel_def_name]
+            bel_colum =f"{bel_def_name}.bel" 
+            pvs[bel_colum] = 0
+            for cf, sign in bel_def.items():
+                # loop through all cashflow components in current bel defintion
+                # TODO move this part to a separate function
+                cf_col = f'{bel_def_name}.{cf}'
+                pvs[bel_colum] += pvs[cf_col] * sign
+
+        return pvs 
+
 
     @staticmethod
     def standardize_column_name(raw_name: str) -> str:
--- src/main.py
+++ src/main.py
@@ -1,135 +1,38 @@
 #! /usr/bin/env python3
 
 # from gooey import Gooey, GooeyParser
-from utils.utils import logger, read_config
-from utils.pv_calcs import calc_pv
-from utils.fmt import map_with_wildcard
-import pandas as pd
-from apps.cfs import CFs
+from argparse import ArgumentParser
+# from gooey import Gooey, GooeyParser
+from run import run_programs
 
 
 # @Gooey(program_name='sample program')
-# def main():
-
-#     logger.info('application started')
-#     main_parser = GooeyParser()
-#     main_parser.add_argument(
-#         'command', help='call a command', type=str, default='gui', nargs='?')
-#     main_parser.add_argument('--config', help='config file for command')
-#     main_args = main_parser.parse_args()
-
-#     if main_args.command == 'gui':
-#         print('launching gui program')
-#     elif main_args.command == 'app1':
-#         print(f'running app1 with config {main_args.config}')
-#     else:
-#         print(f'running app2 with config {main_args.config}')
+def main_gui():
+    pass
 
 
-def test():
-    logger.info(f"Running {__file__}")
+def main():
+    '''main program with subcommand'''
+    parser = ArgumentParser()
+    # Add a subcommand
+    subparsers = parser.add_subparsers(title='subcommands', dest='subcommand')
 
-    config = read_config("configs/my_config.json")
-    logger.info("config file loaded")
-    logger.info(config["server"])
-    logger.info(config.db)
-    logger.info(config["table"])
+    # Create the parser for the cfs subcommand
+    parser_cfs = subparsers.add_parser('cfs', help='calculate pvs for stochastic cashflows')
+    # Add a required argument to the "cfs" subcommand
+    parser_cfs.add_argument('--config', help='cfs parameters in josn format', required=True)
 
-    logger.info(f"Finished running {__file__}")
+    # Create the parser for the "b" subcommand
+    parser_fmt = subparsers.add_parser('fmt', help='construct fmt tables')
+    # Add a required argument to the "b" subcommand
+    parser_fmt.add_argument('--config', help='optional config for fmt')
 
+    # Parse the arguments
+    args = parser.parse_args()
 
-def test_pv():
-    data = {
-        "t": [0, 1, 2, 3, 4, 5],
-        "premiums": [1000, 0, 0, 0, 0, 0],
-        "claims": [0, 100, 100, 100, 100, 200],
-        "discount_factors": [1, 0.99, 0.95, 0.9, 0.8, 0.75],
-    }
-    df = pd.DataFrame(data=data)
-    pvs = calc_pv(df[["premiums", "claims"]], df["discount_factors"])
-    print(pvs)
-    print(pvs.premiums)
-    print(pvs["claims"])
-
-    # print("Validation:")
-    # pv = 0
-    # for claim, discount_factor in zip(df.claims, df.discount_factors):
-    #     pv += claim * discount_factor
-    #     print(claim, discount_factor, claim * discount_factor, pv)
-
-
-def test_fmt_mapping():
-    # test data
-    data_repeat = 100_000
-    cell_tags_data = {
-        "tag_plan_code": ["EI110", "EI125", "EI170"] * data_repeat,
-        "tag_gender": ["M", "F", "M"] * data_repeat,
-        "tag_risk_class": ["P", "s", "N"] * data_repeat,
-        "table_premium": ["", "", ""] * data_repeat,
-        "table_min_premium": ["", "", ""] * data_repeat,
-    }
-
-    premium_fmt_data = {
-        "tag_plan_code": ["EI110"] * 6 + ["EI125"] + ["EI170"],
-        "tag_gender": ["M"] * 3 + ["F"] * 3 + [".*"] * 2,
-        "tag_risk_class": ["P", "S", "N"] * 2 + [".*"] * 2,
-    }
-
-    premium_fmt_data["table_premium"] = [
-        f"Premiums_{p}_{g}_{r}"
-        for p, g, r in zip(
-            premium_fmt_data["tag_plan_code"],
-            premium_fmt_data["tag_gender"],
-            premium_fmt_data["tag_risk_class"],
-        )
-    ]
-
-    premium_fmt_data["table_min_premium"] = [
-        f"Min_Premiums_{p}_{g}_{r}"
-        for p, g, r in zip(
-            premium_fmt_data["tag_plan_code"],
-            premium_fmt_data["tag_gender"],
-            premium_fmt_data["tag_risk_class"],
-        )
-    ]
-
-    cell_tags = pd.DataFrame(data=cell_tags_data)
-    premium_fmt = pd.DataFrame(data=premium_fmt_data)
-
-    # print(cell_tags.head())
-    # print(premium_fmt)
-
-    cells_df = map_with_wildcard(cells_table=cell_tags, fmt_table=premium_fmt)
-    print(cells_df.tail())
-
-
-def test_io():
-    # read data from excel
-    fmt_pf = pd.read_excel("temp/fmt.xlsx", sheet_name="reserves")
-    print(fmt_pf)
-
-
-def test_cfs():
-    params = read_config("configs/cfs_config.json")
-    df = pd.DataFrame(data=pd.read_csv("temp/ifrs17_cfs.csv"))
-    cfs = CFs(params, df)
-    cfs.calc_equivalent_weights()
-    cfs.calc_ess_cfs()
-    print(cfs.df[cfs.df["t"] < 5].ess_weights)
-    print(cfs.df[cfs.df["scenario_no"] == -1])
-    res = cfs.calc_pvs()
-    print(res)
-    print("result of ess: ", f'{res.loc[-1, "premiums_renewal"]:,.2f}')
-    print(
-        "result of stochastic scenarios:",
-        f'{0.5 * (res.loc[1, "premiums_renewal"] + res.loc[2, "premiums_renewal"]): ,.2f}',
-    )
-    print(cfs.df.columns)
-    print(cfs.df.head())
-    print(cfs.df.tail())
-
+    run_programs(args.subcommand, args.config)
 
 if __name__ == "__main__":
     # test_fmt_mapping()
     # test_io()
-    test_cfs()
+    main()
